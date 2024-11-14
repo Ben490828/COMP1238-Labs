04f4aa1dbfc39a64c84acd01611385be0d951fd8
Ben490828 <benjaminpenafiel04@gmail.com>
2024-11-14 11:42:17 -0500

    added ";"

--- Lab11.sql
+++ Lab11.sql
@@ -18,23 +18,23 @@ Order by due_date;
 ----------------------------------
 Select title, due_date
 FROM assignments
-WHERE course_id like 'COMP1234%'
+WHERE course_id like 'COMP1234%';
 ----------------------------------
 SELECT min(due_date)
-FROM assignments
+FROM assignments;
 ----------------------------------
 SELECT max(due_date)
-FROM assignments
+FROM assignments;
 ----------------------------------
 SELECT title, course_id
 FROM assignments
-WHERE due_date like '2024-10-08%'
+WHERE due_date like '2024-10-08%';
 ----------------------------------
 SELECT title, due_date
 FROM assignments
-WHERE due_date like '2024-10%'
+WHERE due_date like '2024-10%';
 ----------------------------------
 select max(due_date)
 FROM assignments
 WHERE status == 'Completed'
-	AND due_date < '2024-11-14'
+	AND due_date < '2024-11-14';
